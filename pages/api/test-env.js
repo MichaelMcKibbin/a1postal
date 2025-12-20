6543fdae8ee081f8de9daa98b2c27b590233c34a
MichaelMcKibbin <74902185+MichaelMcKibbin@users.noreply.github.com>
2025-12-20 06:54:08 +0000

    debug: contact form debugging fail to send (test-env.js)

--- pages/api/test-env.js
+++ pages/api/test-env.js
@@ -1,9 +1,11 @@
 export default function handler(req, res) {
+    const recaptchaSecret = process.env.RECAPTCHA_SECRET_KEY?.replace(/["']/g, '');
+    
     res.status(200).json({
         hasRecaptchaSecret: !!process.env.RECAPTCHA_SECRET_KEY,
+        recaptchaSecretRaw: process.env.RECAPTCHA_SECRET_KEY,
+        recaptchaSecretCleaned: recaptchaSecret,
         hasEmailHost: !!process.env.EMAIL_HOST,
-        hasEmailUser: !!process.env.EMAIL_USER,
-        hasEmailPass: !!process.env.EMAIL_PASS,
         nodeEnv: process.env.NODE_ENV
     });
 }

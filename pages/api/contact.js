166386932aaa15e086646f092350e448a62c8885
MichaelMcKibbin <74902185+MichaelMcKibbin@users.noreply.github.com>
2025-12-21 05:19:26 +0000

    debug: contact form debugging fail to send - debugging recaptcha failure on live site

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -35,11 +35,14 @@ export default async function handler(req, res) {
 
         const transporter = nodemailer.createTransport({
             host: process.env.EMAIL_HOST,
-            port: process.env.EMAIL_PORT,
-            secure: false,
+            port: parseInt(process.env.EMAIL_PORT),
+            secure: process.env.EMAIL_PORT == '465',
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
+            },
+            tls: {
+                rejectUnauthorized: false
             }
         });
 
